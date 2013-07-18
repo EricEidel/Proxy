52b4a75059eab24af622ffe9253843afd66744e6
Steven Huang <apexflash.com@gmail.com>
2013-07-18 11:22:17 -0600

    updated old version

--- Non-Thread-version/src/old/Proxy.java
+++ Non-Thread-version/src/old/Proxy.java
@@ -21,198 +21,203 @@ public class Proxy
 {
 	static final String CRLF = "\r\n";
 	static String host_name;
-	private static int count;
+	//private static int count;
 	private static int bSize;
-	private static byte[] buffer;
+	//private static byte[] buffer;
 	private static Hashtable<String, Socket> hosts;
 	Socket proxySocket;
 	
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws Exception
 	{
-		//Welcome soccet + accept
-		
 		hosts = new Hashtable<String, Socket>();
-
-		ServerSocket welcomeSocket = new ServerSocket(2000);
-		
 		
 		// Communication between BROWSER - PROXY
-		
+		ServerSocket welcomeSocket = new ServerSocket(2000);
 		Socket proxySocket = welcomeSocket.accept();
 		
 		while (true)
 		{
 			try
+			{
+				//check status of proxySocket for new cycle
+				if (proxySocket.isClosed())
 				{
-					if (proxySocket.isClosed())
+					System.out.println("\n=== NEW proxySocket ===\n");
+					proxySocket= welcomeSocket.accept();
+				}
+				else
+				{
+					System.out.println("\n=== RE-USED proxySocket ===\n");
+				}
+				// initialize
+				//count = 0;
+				bSize = 16384;
+				
+				//create reader for requests from browser
+				BufferedReader browserToProxyReader = new BufferedReader(new InputStreamReader(proxySocket.getInputStream()));
+				
+				int port = -1; 	// requested port
+				host_name = "";	// requested host
+				
+				//first line of request header
+				String httpRequest = browserToProxyReader.readLine();
+		
+				if (httpRequest != null)
+				{
+					// Parse the host name.
+					host_name = parseHost(httpRequest);
+					port = 80;
+					
+					System.out.println("=== BROWSER REQUEST ===");
+					
+					// Gets all the headers as strings and adds them to the list. 
+					// NOTE: If host header is in there, it changes host_name to the host header.
+					ArrayList<String> list = new ArrayList<String>();
+					list = get_message(httpRequest, browserToProxyReader);
+					
+					// between PROXY - SERVER
+					Socket toServer;
+					
+					// check hosts hash table
+					if (hosts.containsKey(host_name))
 					{
-						System.out.println("\nNEW WELCOME SOCKET ACCEPT.");
-						proxySocket= welcomeSocket.accept();
-					}
-					else
-					{
-						System.out.println("\nRE-USED!");
-					}
-					// initialize
-					count = 0;
-					bSize = 1000;
-					
-					BufferedReader browserToProxyReader = new BufferedReader(new InputStreamReader(proxySocket.getInputStream()));
-					//DataOutputStream outToBrowser = new DataOutputStream(proxySocket.getOutputStream());
-					//OutputStreamWriter proxyToBrowserWriter = new OutputStreamWriter(outToBrowser);
-					
-					int port = -1; 	// The port the request is going to
-					host_name = "";	// The host the request is going to
-					
-					
-					String httpRequest = browserToProxyReader.readLine();
-			
-					if (httpRequest != null)
-					{
-						// Parse the host name.
-						host_name = parseHost(httpRequest);
-						port = 80;
-						
-						//System.out.println("MESSAGE FROM BROWSER TO PROXY:");
-						
-						// Gets all the headers as strings and adds them to the list. 
-						// NOTE: If host header is in there, it changes host_name to the host header.
-						ArrayList<String> list = new ArrayList<String>();
-						list = get_message(httpRequest, browserToProxyReader);
-						
-						// FINISH READING MESSAGE FROM BROWSER TO PROXY
-						System.out.println("HOST: " + host_name);
-						//System.out.println("PORT: " + port);
-						
-						Socket toServer;
-						
-						//This is the channel for PROXY - WEBSERVER communication
-						if (hosts.containsKey(host_name))
+						System.out.println("=== Old host found ===");
+						toServer = hosts.get(host_name);
+						if (toServer.isClosed())
 						{
-							System.out.println("Old host used!");
-							toServer = hosts.get(host_name);
-							if (toServer.isClosed())
-							{
-								System.out.println("Host was closed!");
-								toServer = new Socket(host_name, port);
-								hosts.put(host_name,toServer);
-							}
-						}
-						else
-						{
-							System.out.println("New hosts created!");
+							System.out.println("=== Host was closed ===");
 							toServer = new Socket(host_name, port);
 							hosts.put(host_name,toServer);
 						}
-						
-						
-						
-						//BufferedReader webserverToProxyReader = new BufferedReader(new InputStreamReader(toServer.getInputStream())); // This is where the server answers the request
-						OutputStreamWriter proxyToWebserverWriter = new OutputStreamWriter(new DataOutputStream(toServer.getOutputStream()));
-						
-						System.out.println("MSG FROM PROXY TO SERVER:");
-						
-						// Fowards the message recieved from the browser to the web-server
-						send_message_to_server(list, proxyToWebserverWriter);
-						
-						// Reads the response from the web-server and sends it back to the browser
-			
-				       	buffer = new byte[bSize];
-				       	// Writer to the proxy-browser
-				        BufferedOutputStream myWriter = new BufferedOutputStream(proxySocket.getOutputStream());
-				        // Reader from the server-proxy.
-				        BufferedInputStream myReader =  new BufferedInputStream(toServer.getInputStream(),1024);
-						//System.out.println("\nFINISHED A CYCLE");
-						
-						int b; // the byte read from the file
-				        while ((b = myReader.read( )) != -1) 
-				        {
-				    		BufferWrite(b, myWriter);
-				    		//System.out.print((char)b);
-				        }
-				        flush(myWriter);
-				        
-				        myReader.close();
-				        myWriter.close();
-				        //proxySocket.close();
 					}
+					else
+					{
+						System.out.println("=== New hosts created ===");
+						toServer = new Socket(host_name, port);
+						hosts.put(host_name,toServer);
+					}
+					
+					
+					// writes requests from proxy to server
+					OutputStreamWriter proxyToWebserverWriter = new OutputStreamWriter(new DataOutputStream(toServer.getOutputStream()));
+					
+					System.out.println("=== PROXY TO SERVER ===");
+					
+					// Forwards the request received from the browser to the web-server
+					send_message_to_server(list, proxyToWebserverWriter);
+					
+					
+					
+					// Reads the response from the web-server and sends it back to the browser
+					
+					// server response reader (server -> proxy)
+					BufferedInputStream serverResponseReader =  new BufferedInputStream(toServer.getInputStream(),bSize);
+					
+					// browser response writer (proxy -> browser)
+					BufferedOutputStream proxyToBrowserWriter = new BufferedOutputStream(proxySocket.getOutputStream());
+					
+					System.out.println("=== WAITING FOR SERVER ===");
+					
+					byte[] buffer = new byte[bSize];
+					int bytesRead = serverResponseReader.read(buffer, 0, bSize);
+					while (bytesRead != -1)
+					{
+						proxyToBrowserWriter.write(buffer, 0, bytesRead);
+						bytesRead = serverResponseReader.read(buffer, 0, bSize);
+					}
+					proxyToBrowserWriter.flush();
+					
+					
+					/*
+					int b; // the byte read from server
+					while ((b = serverResponseReader.read( )) != -1) 
+					{
+						BufferWrite(b, proxyToBrowserWriter);
+						//System.out.print((char)b);
+					}
+					System.out.println("=== WRITE TO BROWSER ===");
+					flush(proxyToBrowserWriter);
+					proxyToBrowserWriter.flush();
+					*/
+					
+					serverResponseReader.close();
+					proxyToBrowserWriter.close();
+					
+					System.out.println("=== REQUEST COMPLETED ===");
+					System.gc(); // suggest garbage collection
 				}
-			catch (Exception e)
+				
+				else
 				{
-					System.out.println("An error has occured!");
-					e.printStackTrace();
+					//System.out.println("=== REQUEST NULL, RESET proxySocket ===");
 					proxySocket= welcomeSocket.accept();
 				}
+			}
+			
+			catch (Exception e)
+			{
+				System.out.println("=== ERROR, RESET proxySocket === ");
+				e.printStackTrace();
+				proxySocket= welcomeSocket.accept();
+			}
+			
+		}// end of infinite while loop
+	}
+	
+	/*
+	private static void flush(BufferedOutputStream proxyToBrowserWriter) throws Exception
+	{
+		if(count > 0)
+		{
+			byte[] temp = Arrays.copyOfRange(buffer,0,count);
+			
+			proxyToBrowserWriter.write(temp);
+			
 		}
 	}
 	
-	private static void flush(BufferedOutputStream myWriter) 
+	private static void BufferWrite(int b, BufferedOutputStream proxyToBrowserWriter) throws Exception
 	{
-	if(count > 0)
-	    {
-	        byte[] temp = Arrays.copyOfRange(buffer,0,count);
-	        try 
-	        {
-	        	myWriter.write(temp);
-	        } 
-	        catch (IOException e) 
-	        {
-	          System.out.println("Error in writing to file");
-	          e.printStackTrace();
-	        }
-	    }
-	}
-
-	private static void BufferWrite(int b, BufferedOutputStream myWriter)
-	{
-	if(count < bSize)
-	{
-	    buffer[count] = (byte)b;
-	    count++;
-	}
-	
-	if(count == bSize)
-	{
-	    try 
-	    {
-	        myWriter.write(buffer);
-	    } 
-	    catch (IOException e) 
-	    {
-	        System.out.println("Error in writing to file");
-	    }
-	    count = 0;
-	    for(byte t:buffer)
-	        t = 0;
-	}
-	
-	}
+		if(count < bSize)
+		{
+			buffer[count] = (byte)b;
+			count++;
+		}
+		
+		if(count == bSize)
+		{
+			proxyToBrowserWriter.write(buffer);
+			count = 0;
+			buffer = new byte[bSize];
+		}
+		
+	}*/
 
 
-	// This method simpley sends all the string list has to the web server
+	// This method simply sends all the string list has to the web server
 	private static void send_message_to_server(ArrayList<String> list, OutputStreamWriter proxyToWebserverWriter) throws Exception
 	{
 		for (String s: list)
 		{
 			proxyToWebserverWriter.append(s);
-			System.out.print(s);
 		}
 		
 		proxyToWebserverWriter.flush();		
 	}
 
-	/* This method appends to list all the headers from a request off the browser. 
+	/* This method appends to list all the headers from a request off the browser, print to console. 
 	*  If there's a host header, it changes host_name to it's value.
 	*/
 	private static ArrayList<String> get_message(String httpRequest, BufferedReader browserToProxyReader) throws Exception
 	{
 		ArrayList<String> list = new ArrayList<String>();
 		
+		//keep reading from browser until empty line
 		while (!httpRequest.equals(""))
 		{
 			if (httpRequest.contains("Host:"))
-					host_name = httpRequest.split(" ")[1];
+				host_name = httpRequest.split(" ")[1];
 					
 			//if (httpRequest.contains("Accept-Encoding:"))
 				//="Accept-Encoding: None";
@@ -223,9 +228,7 @@ public class Proxy
 			httpRequest = browserToProxyReader.readLine();
 		}
 		list.add(CRLF);
-		list.add("Connection: Keep-Alive");
 		list.add(CRLF);
-		System.out.println();
 		
 		return list;
 	}
@@ -256,9 +259,9 @@ public class Proxy
 		}	
 		else
 		{
-			System.out.println("This isn't a get request!");
+			System.out.println("=== NOT GET request === ");
 			System.out.println(httpRequest);
-			throw (new Exception());
+			throw (new Exception("None GET request"));
 		}	
 		return host_name;
 	}
